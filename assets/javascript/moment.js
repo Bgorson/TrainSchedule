6537b7213208af50f4f18da7c984b962a40247ce
BGorson <BGorson32@gmail.com>
2019-01-18 16:11:31 -0600

    added remove train bttn

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,67 +1,76 @@
 // JScript
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBBWhCmujtkg4mwIzjcZGBlpdWlKPo5r9M",
-    authDomain: "train-scheduler-1e9ee.firebaseapp.com",
-    databaseURL: "https://train-scheduler-1e9ee.firebaseio.com",
-    projectId: "train-scheduler-1e9ee",
-    storageBucket: "train-scheduler-1e9ee.appspot.com",
-    messagingSenderId: "840854154538"
-  };
-  firebase.initializeApp(config);
-  var database= firebase.database();
-  var trainInfo= [];
+// Initialize Firebase
+var trainInfo = [];
+var config = {
+  apiKey: "AIzaSyBBWhCmujtkg4mwIzjcZGBlpdWlKPo5r9M",
+  authDomain: "train-scheduler-1e9ee.firebaseapp.com",
+  databaseURL: "https://train-scheduler-1e9ee.firebaseio.com",
+  projectId: "train-scheduler-1e9ee",
+  storageBucket: "train-scheduler-1e9ee.appspot.com",
+  messagingSenderId: "840854154538"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+database.ref().on("child_added", function (snapshot) {
+  console.log(snapshot.key)
+  var trainFrequency = snapshot.val().frequency;
+  var startMinutes = parseInt((snapshot.val().firstTrain.slice(0,2)))*60 + (parseInt(snapshot.val().firstTrain.slice(3,5)))
+  var now = moment().format("HH:mm")
+  var nowMinutes= parseInt((now.slice(0,2)))*60 + (parseInt(now.slice(3,5)))
+  var minutesAway= trainFrequency-((nowMinutes- startMinutes) % trainFrequency) 
+  var nextArrivalHours = (Math.floor(nowMinutes + minutesAway)/60).toFixed(0)
+  console.log(nowMinutes + minutesAway)
+  var nextArrivalMinutes = ((nowMinutes+minutesAway)%60)
+  if ((parseInt(nextArrivalMinutes)) <10){
+    nextArrivalMinutes ='0'+ nextArrivalMinutes
+  }
+
+  var nextArrival= (nextArrivalHours+":"+nextArrivalMinutes)
+  var closeButton = $("<button type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>")
+  var newRow = $("<tr value=" + snapshot.key + ">")
+  var newName = $("<td>" + snapshot.val().name + "</td>")
+  var newDestination = $("<td>" + snapshot.val().destination + "</td>")
+  var newTime = $("<td>" + nextArrival + "</td>")
+  var newNext = $("<td>" + trainFrequency + "</td>")
+  var newMins = $("<td>" + minutesAway + "</td>")
+  $(newRow).append(newName,newDestination,newNext,newTime,newMins,closeButton)
+  $("tbody").append(newRow)
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+  // Get the input values
+  var newTrainInfo = {
+    name: $("#trainNameForm").val().trim(),
+    destination: $("#destinationForm").val().trim(),
+    firstTrain: $("#firstTrainForm").val().trim(),
+    frequency: $("#frequencyForm").val().trim()
+  }
+  trainInfo.push(newTrainInfo)
+  console.log(trainInfo);
+  database.ref().push(newTrainInfo);
+})
+function removeItem(item) {
+  // Now we can get back to that item we just pushed via .child().
+  item.remove(function() {
+   
+  });
+}
+//deleting data
+$(document).on("click", ".close", function(e){
+  var key = $(this).parent().attr('value');
+  console.log(key)
+  $("[value="+key+ "]").remove()
+  database.ref().child(key).remove();
+
+  })
   
-  database.ref().on("value", function(snapshot) { 
-    $("tbody").html('');
-    trainInfo = snapshot.val().trainInfo;
-      for(i=0;i< snapshot.val().trainInfo.length;i++){  
-    console.log(snapshot.val())
-    var trainName = snapshot.val().trainInfo[i].name;
-    var trainDestination= snapshot.val().trainInfo[i].destination;
-    var trainFirst = snapshot.val().trainInfo[i].firstTrain;
-    var trainFrequency = snapshot.val().trainInfo[i].frequency;
-    console.log(trainDestination)
-    console.log(trainName)
-    var newRow= $("<tr>")
-    var newName= $("<td>" + trainName + "</td>" )
-    var newDestination= $("<td>" + trainDestination + "</td>")
-    var newTime= $("<td>" + trainFirst + "</td>")
-    var newNext= $("<td>" + trainFrequency + "</td>")
-    var newMins= $("<td>" + trainFrequency + "</td>")
-    $(newRow).append(newName)
-    $(newRow).append(newDestination)
-    $(newRow).append(newNext)
-    $(newRow).append(newTime)
-    $(newRow).append(newMins)
-    $("tbody").append(newRow)
-        }},function(errorObject) {
-            console.log("The read failed: " + errorObject.code);
-          });
-
-
-//Calculating next train time
-// function nextTrain(frequency){
-//   var ts = new Date();
-//   ts = ts.toLocaleTimeString();
-
-// }
-
-$("#submit").on("click", function(event) {
-  $("tbody").html('');
-            event.preventDefault();
-            // Get the input values
-            var newTrainInfo= {
-                name: $("#trainNameForm").val().trim(),
-                destination: $("#destinationForm").val().trim() ,
-                firstTrain:$("#firstTrainForm").val().trim() ,
-                frequency: $("#frequencyForm").val().trim()
-            }
-            trainInfo.push(newTrainInfo)
-            console.log(trainInfo);
-          
-        
-              database.ref().set({
-                trainInfo: trainInfo,
-              });
-            })
+  database.ref().on("value", function (snapshot) {
+    console.log(snapshot.val().name)
+  })
+    
+    
